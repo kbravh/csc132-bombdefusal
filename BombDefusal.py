827c4126e9335f3b5cdf60aa4998e3c486df1874
Karey Higuera <karey.higuera@gmail.com>
2019-03-31 12:56:38 -0500

    Add module list for keeping track of completion

--- BombDefusal.py
+++ BombDefusal.py
@@ -1,14 +1,16 @@
-#####################################################
-# Name: Alex Anderson, Shawn Chauvin, Karey Higuera #
-# Description: A real life implementation of KTANE  #
-#       (Keep Talking and Nobody Explodes)          #
-#####################################################
+######################################################
+#           Project: RaspPi Bomb Defusal             #
+# Names: Alex Anderson, Shawn Chauvin, Karey Higuera #
+# Description: A real life implementation of KTANE   #
+#       (Keep Talking and Nobody Explodes)           #
+######################################################
 
 #Base class for the bomb. This will handle all major game functions.
 class Bomb(object):
     def __init__(self, timer=60):
         self.timer = timer
         self.strikes = 0
+        self.modules = [0,0,0]
 
     @property
     def timer(self):
@@ -17,6 +19,10 @@ class Bomb(object):
     @property
     def strikes(self):
         return self._strikes
+
+    @property
+    def modules(self):
+        return self._modules
 
     @timer.setter
     def timer(self, time):
@@ -29,13 +35,41 @@ class Bomb(object):
         if (self._strikes >= 3):
             self.explode()
 
+    @modules.setter
+    def modules(self, modules):
+        self._modules = modules
+        #if all the modules are solved, finish the game
+        if self._modules == [1,1,1]:
+            self.win()
+
+    def moduleComplete(self, modNumber):
+        #pull a copy of the list of module states
+        updatedModules = self.modules
+        #set the indicated module to complete
+        updatedModules[modNumber] = 1
+        #push this updated list to the Bomb
+        self.modules = updatedModules
+
     def explode(self):
         print "BOOM!"
 
-#Abstract module class. All sub-modules extend this.
+    def win(self):
+        print "You win!"
+
+#Abstract Module class. All sub-modules extend this.
 class Module(object):
-    def __init__(self):
-        pass
+
+    def __init__(self, modNumber):
+        """This modNumber is the spot in the Bomb modules array that marks whether this module is complete or not."""
+        self._modNumber = modNumber
+
+    @property
+    def modNumber(self):
+        return self._modNumber
+
+    @modNumber.setter
+    def modNumber(self, modNumber):
+        self._modNumber = modNumber
 
     #add a strike to the main bomb instance
     def strike(self, bomb):
