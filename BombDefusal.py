6a89b0455dd8583f64c9d19870b94d2ad7cca6eb
Karey Higuera <karey.higuera@gmail.com>
2019-04-08 13:11:31 -0500

    Build gameSetup function and add toggle for raspPi

--- BombDefusal.py
+++ BombDefusal.py
@@ -4,15 +4,33 @@
 # Description: A real life implementation of KTANE   #
 #       (Keep Talking and Nobody Explodes)           #
 ######################################################
+"""Whether or not running on a raspberryPi, if false
+    will disable the Pi specific functionality for testing"""
+raspberryPi = False
+
 import time
 import datetime
+import abc
 
 from Adafruit_LED_Backpack import SevenSegment
 ######################################################
-segment = SevenSegment.SevenSegment(address=0x70)
 
-# Initialize the display. Must be called once before using the display.
-segment.begin()
+if raspberryPi:
+    segment = SevenSegment.SevenSegment(address=0x70)
+
+    # Initialize the display. Must be called once before using the display.
+    segment.begin()
+
+#the bomb container for all games
+bomb = None
+#the modules containers for all games
+module1 = None
+module2 = None
+module3 = None
+
+#############
+###CLASSES###
+#############
 
 #Base class for the bomb. This will handle all major game functions.
 class Bomb(object):
@@ -67,25 +85,27 @@ class Bomb(object):
 
     def explode(self):
         print ("BOOM!")
-        #flash the timer on and off
         while(True):
-            segment.set_digit(0, 0)
-            segment.set_digit(1, 0)
-            segment.set_digit(2, 0)
-            segment.set_digit(3, 0)
-            segment.set_colon(True)
-            segment.write_display()
-            time.sleep(0.5)
+            if raspberryPi:
+            #flash the timer on and off
+                segment.set_digit(0, 0)
+                segment.set_digit(1, 0)
+                segment.set_digit(2, 0)
+                segment.set_digit(3, 0)
+                segment.set_colon(True)
+                segment.write_display()
+                time.sleep(0.5)
 
-            segment.clear()
-            segment.write_display()
-            time.sleep(0.5)
+                segment.clear()
+                segment.write_display()
+                time.sleep(0.5)
 
     def win(self):
         print ("You win!")
 
 #Abstract Module class. All sub-modules extend this.
 class Module(object):
+    __metaclass__ = abc.ABCMeta
 
     def __init__(self, modNumber, bomb):
         #This modNumber is the spot in the Bomb modules array that marks whether this module is complete or not.
@@ -117,7 +137,17 @@ class Module(object):
     def solve(self):
         self.bomb.moduleComplete(self.modNumber)
 
+    #abstract method to determine if a module is complete
+    #each module type will need to define this
+    @abc.abstractmethod
+    def checkModule(self):
+        """This determines the solved state of a module"""
 
+###################
+###OTHER METHODS###
+###################
+
+#this method writes the remaining time to the screen
 def writeToClock(minutes, seconds, hundSecs):
     segment.clear()    
 
@@ -150,8 +180,23 @@ def writeToClock(minutes, seconds, hundSecs):
     # update the actual display LEDs.
     segment.write_display()
 
+#this method initializes the bomb
+#this is triggered by "New Game" or "Try Again" button
+''' this could be extended to support multiple levels/configs
+    based on parameters passed in'''
+def gameSetup():
+    global bomb
+    global module1, module2, module3
+    bomb = Bomb(15)
+    module1 = Module(1, bomb)
+    module2 = Module(2, bomb)
+    module3 = Module(3, bomb)
+
+#runs the gameplay
+#this is triggered by the "Start" button
 def playGame():
     while(True):
+        ###TIMER###
         #this is the time right now
         currentTime = datetime.datetime.now()
         #this is the total number of seconds that have gone by
@@ -160,7 +205,8 @@ def playGame():
         timeLeft = bomb.timer - timeDiff
         
         if(timeLeft <= 0):
-            writeToClock(0,0,0)
+            if raspberryPi:
+                writeToClock(0,0,0)
             bomb.explode()
 
         else:
@@ -172,11 +218,15 @@ def playGame():
             hundSecs = str(round(timeLeft%1, 2))[2:4]
             print("time left: {}:{}:{}".format(minutes, seconds, hundSecs))
 
-            writeToClock(minutes, seconds, hundSecs)
+            if raspberryPi:
+                writeToClock(minutes, seconds, hundSecs)
 
-            time.sleep(0.05)
+        ###MODULES###
+        #check the state of each module
 
-##SAMPLE CODE##
-bomb = Bomb(15)
+        time.sleep(0.05)
+
+
+gameSetup()
 
 bomb.startBomb()
