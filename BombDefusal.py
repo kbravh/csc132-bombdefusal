b25be04586ff9bbb4d1b3c455bbc99220e394fae
Karey Higuera <karey.higuera@gmail.com>
2019-05-01 08:43:14 -0500

    add keypad functionality

--- BombDefusal.py
+++ BombDefusal.py
@@ -47,15 +47,16 @@ secondWireConfig = {
     'wiresToLeave' : [wire1]
 }
 
-#Setup for the keypad
-cols = [digitalio.DigitalInOut(x) for x in (board.D9, board.D6, board.D5)]
-rows = [digitalio.DigitalInOut(x) for x in (board.D13, board.D12, board.D11, board.D10)]
-keys = ((1, 2, 3),
-        (4, 5, 6),
-        (7, 8, 9),
-        ('*', 0, '#'))
+if raspberryPi:
+    #Setup for the keypad
+    cols = [digitalio.DigitalInOut(x) for x in (board.D9, board.D6, board.D5)]
+    rows = [digitalio.DigitalInOut(x) for x in (board.D13, board.D12, board.D11, board.D10)]
+    keys = ((1, 2, 3),
+            (4, 5, 6),
+            (7, 8, 9),
+            ('*', 0, '#'))
 
-keypad = adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keys)
+    keypad = adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keys)
 
 #basic object for keypad module
 defaultKeypadConfig = {
@@ -277,7 +278,7 @@ class Keypad(Module):
 
     def checkModule(self):
         #all keypresses after module is solved will be ignored
-        if (not self.solved):
+        if (raspberryPi and not self.solved):
             #pulled the currently pressed keys from the keypad
             #this is an array
             keys = keypad.pressed_keys
