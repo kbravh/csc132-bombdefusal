e270f90b11c552b7ab6e3465cd47e85bc09224c2
Karey Higuera <karey.higuera@gmail.com>
2019-05-15 09:48:44 -0500

    add keypad configurations

--- configs.py
+++ configs.py
@@ -10,37 +10,35 @@ wire2 = {'pin': wirePin2}
 wire3 = {'pin': wirePin3}
 
 
-wireConfigVowel = {
-    'wire1' : wire1,
-    'wire2' : wire2,
-    'wire3' : wire3,
-    'wiresToSolve' : [wire1],
-    'wiresToLeave' : [wire2, wire3],
-    'type': 'vowel'
-}
-wireConfigOdd = {
-    'wire1' : wire1,
-    'wire2' : wire2,
-    'wire3' : wire3,
-    'wiresToSolve' : [wire1, wire3],
-    'wiresToLeave' : [wire2],
-    'type': 'odd'
-}
-wireConfigEven = {
-    'wire1' : wire1,
-    'wire2' : wire2,
-    'wire3' : wire3,
-    'wiresToSolve' : [wire1, wire2],
-    'wiresToLeave' : [wire3],
-    'type': 'even'
-}
+wireConfigs = [
+    {
+        'wire1' : wire1,
+        'wire2' : wire2,
+        'wire3' : wire3,
+        'wiresToSolve' : [wire1],
+        'wiresToLeave' : [wire2, wire3],
+        'type': 'vowel'
+    },
+    {
+        'wire1' : wire1,
+        'wire2' : wire2,
+        'wire3' : wire3,
+        'wiresToSolve' : [wire1, wire3],
+        'wiresToLeave' : [wire2],
+        'type': 'odd'
+    },
+    {
+        'wire1' : wire1,
+        'wire2' : wire2,
+        'wire3' : wire3,
+        'wiresToSolve' : [wire1, wire2],
+        'wiresToLeave' : [wire3],
+        'type': 'even'
+    }
+]
 
 #the basic wire setup for Cut The Wires
-defaultWireConfig = wireConfigVowel
-
-wireConfigs = [
-    wireConfigVowel, wireConfigOdd, wireConfigEven
-]
+defaultWireConfig = wireConfigs.wireConfigVowel
 
 vowelSerialNumbers = [
     'XJD45E23',
@@ -75,63 +73,94 @@ defaultKeypadConfig = {
     'sequence' : "43556"
 }
 
-keypadConfig1 = {
-    'word' : "JANKY",
-    "hint" : "KNEW",
-    'sequence' : "51659"
-}
-
-keypadConfig2 = {
-    'word' : "MEMES",
-    "hint" : "NEW",
-    'sequence' : "63637"
-}
-
-keypadConfig3 = {
-    'word' : "JOCKO",
-    "hint" : "BLUE",
-    'sequence' : "56256"
-}
-
-keypadConfig4 = {
-    'word' : "WUMBO",
-    "hint" : "BLEW",
-    'sequence' : "98626"
-}
-
-keypadConfig5 = {
-    'word' : "KOPIE",
-    "hint" : "FLU",
-    'sequence' : "56743"
-}
-
-keypadConfig6 = {
-    'word' : "MUSIK",
-    "hint" : "FLEW",
-    'sequence' : "68745"
-}
-
-keypadConfig7 = {
-    'word' : "HUYAM",
-    "hint" : "TO",
-    'sequence' : "48926"
-}
-
-keypadConfig8 = {
-    'word' : "PUJAH",
-    "hint" : "TOO",
-    'sequence' : "78524"
-}
-
-keypadConfig9 = {
-    'word' : "JIGGY",
-    "hint" : "TWO",
-    'sequence' : "54449"
-}
-
 keypadConfigs = [
-    keypadConfig1, keypadConfig2, keypadConfig3, keypadConfig4, keypadConfig5, keypadConfig6, keypadConfig7, keypadConfig8, keypadConfig9
+    {
+        'word' : "JANKY",
+        "hint" : "KNEW",
+        'sequence' : "51659"
+    },
+    {
+        'word' : "MEMES",
+        "hint" : "NEW",
+        'sequence' : "63637"
+    },
+    {
+        'word' : "JOCKO",
+        "hint" : "BLUE",
+        'sequence' : "56256"
+    },
+    {
+        'word' : "WUMBO",
+        "hint" : "BLEW",
+        'sequence' : "98626"
+    },
+    {
+        'word' : "KOPIE",
+        "hint" : "FLU",
+        'sequence' : "56743"
+    },
+    {
+        'word' : "MUSIK",
+        "hint" : "FLEW",
+        'sequence' : "68745"
+    },
+    {
+        'word' : "HUYAM",
+        "hint" : "TO",
+        'sequence' : "48926"
+    },
+    {
+        'word' : "PUJAH",
+        "hint" : "TOO",
+        'sequence' : "78524"
+    },
+    {
+        'word' : "JIGGY",
+        "hint" : "TWO",
+        'sequence' : "54449"
+    },
+    {
+        'word' : "MUSAK",
+        "hint" : "YOURE",
+        'sequence' : "68725"
+    },
+    {
+        'word' : "BROWS",
+        "hint" : "YORE",
+        'sequence' : "27697"
+    },
+    {
+        'word' : "PRISE",
+        "hint" : "UR",
+        'sequence' : "77473"
+    },
+    {
+        'word' : "PRIES",
+        "hint" : "YOUR",
+        'sequence' : "77437"
+    },
+    {
+        'word' : "TERSE",
+        "hint" : "YOU",
+        'sequence' : "83773"
+    },
+    {
+        'word' : "TERCE",
+        "hint" : "U",
+        'sequence' : "83723"
+    },
+    {
+        'word' : "SLOES",
+        "hint" : "EWE",
+        'sequence' : "75637"
+    },
+    {
+        'word' : "SLOWS",
+        "hint" : "EW",
+        'sequence' : "75697"
+    }
 ]
+
 
 ###BUTTON###
 #basic object for button
